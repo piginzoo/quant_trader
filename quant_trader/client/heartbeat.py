ef31c857319545af0823666c9c5ae38db6f1ee6b
piginzoo <piginzoo@gmail.com>
2023-02-08 23:08:24 +0800

    impl a new heart beat service

--- quant_trader/client/heartbeat.py
+++ quant_trader/client/heartbeat.py
@@ -17,9 +17,10 @@ def run():
             full_url = utils.get_url()
             utils.http_json_post(full_url,{"action": "heartbeat","name": "etf"}) # name是为了标识系统
         except Exception:
-            logger.exception("推送[ETF]心跳失败")
+            logger.exception("发送[ETF]心跳失败")
+            continue
 
-        logger.info("发送[ETF]心跳：%s",datetime.datetime.strftime(datetime.datetime.now(), "%Y-%m-%d %H:%M:%S"))
+        # logger.info("发送[ETF]心跳完成：%s",datetime.datetime.strftime(datetime.datetime.now(), "%Y-%m-%d %H:%M:%S"))
         time.sleep(60) # 写死了，懒得再写配置了
 
 if __name__ == '__main__':
