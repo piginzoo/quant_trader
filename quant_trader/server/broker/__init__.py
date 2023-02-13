1b0662faa4bc387e9dcb10a637d319e58d5e49ad
piginzoo <piginzoo@gmail.com>
2023-02-13 20:34:33 +0800

    remove easytrader

--- quant_trader/server/broker/__init__.py
+++ quant_trader/server/broker/__init__.py
@@ -1,9 +1,6 @@
-from quant_trader.server.broker.easytrader_broker import EaseTraderBroker
 from quant_trader.server.broker.qmt_broker import QMTBroker
 
 
 def get(name):
-    if name == "easytrader":
-        return EaseTraderBroker()
     if name == "qmt":
         return QMTBroker()
