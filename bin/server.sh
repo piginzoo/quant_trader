1d60b8c6734ab7357e99c7ab953e41927c694180
piginzoo <piginzoo@gmail.com>
2023-03-06 09:23:58 +0800

    bugfix

--- bin/server.sh
+++ bin/server.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 if [ "$1" = "stop" ]; then
     echo "停止 Web 服务"
-    ps aux|grep quant_trader|grep -v grep|awk '{print $2}'|xargs kill -9
+    pkill -9  -f 'quant_trader.server.server'
     exit
 fi
 
 if [ "$1" = "restart" ]; then
-    echo "停止 Web 服务"
-    ps aux|grep quant_trader|grep -v grep|awk '{print $2}'|xargs kill -9
+    echo "重启服务：停止 Web 服务"
+    pkill -9  -f 'quant_trader.server.server'
     sleep 1
-    echo "启动 Web 服务"
+    echo "重启服务：启动 Web 服务"
     nohup python -m quant_trader.server.server>logs/log.txt 2>&1 &
 fi
 
